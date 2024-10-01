6df6fc9e6ee4edc3a0fa5c5c47d373ea1960cb55
Bruno Martins <bgcngm@gmail.com>
2024-10-01 22:35:45 +0100

    sm8650-common: Address atfwd seccomp filter failure
    Fixes:
     W ATFWD-daemon: libminijail[4334]: logging seccomp filter failures
     E ATFWD-daemon: libminijail[4334]: blocked syscall: gettid
     I init    : Service 'vendor.atfwd' (pid 4334) received signal 31
     I init    : Sending signal 9 to service 'vendor.atfwd' (pid 4334) process group...
    
    Change-Id: Ic259464520db6e77f06a3d0820a63662aa91659c

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s|/system/system_ext|/system_ext|g" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
+            ;;
         odm/lib64/libAlgoProcess.so)
             [ "$2" = "" ] && return 0
             sed -i "s/android.hardware.graphics.common-V3-ndk.so/android.hardware.graphics.common-V6-ndk.so/" "${2}"
