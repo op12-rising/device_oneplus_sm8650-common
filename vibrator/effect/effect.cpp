b3c3df4aefd413f124f1679dc68b439156bad0d0
John Galt <johngaltfirstrun@gmail.com>
2025-06-18 10:10:20 -0400

    vibrator: add all primitive effects
    These are reused from stock bins wherever possible, otherwise generated
    using stock and intended effects as reference.

--- vibrator/effect/effect.cpp
+++ vibrator/effect/effect.cpp
@@ -36,74 +36,9 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
 
 #include "generated_effect.h"
+#include "primitive_effect.h"
 #include <android-base/properties.h>
 #include <string>
-
-static const int8_t primitive_0[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-};
-
-static const int8_t primitive_1[] = {
-    17,  34,  50,  65,  79,  92,  103, 112, 119, 124,
-    127, 127, 126, 122, 116, 108, 98,  86,  73,  58,
-    42,  26,  9,   -8,  -25, -41, -57, -72, -85, -97,
-    -108, -116, -122, -126, -127, -127, -125, -120,
-    -113, -104, -93,  -80, -66, -51, -35, -18, -1,
-};
-
-static const int8_t primitive_1_gentle[] = {
-     0,  10,  16,  22,  28,  33,  38,  43,  47,  51,
-    55,  59,  62,  65,  68,  71,  73,  75,  77,  78,
-    79,  80,  80,  80,  80,  79,  78,  77,  75,  73,
-    71,  68,  65,  62,  59,  55,  51,  47,  43,  38,
-    33,  28,  22,  16,  10,   5,   0,  -5, -10
-};
-
-static const struct effect_stream primitives[] = {
-    {
-        .effect_id = 0,
-        .length = ARRAY_SIZE(primitive_0),
-        .play_rate_hz = 8000,
-        .data = primitive_0,
-    },
-
-    {
-        .effect_id = 1,
-        .length = ARRAY_SIZE(primitive_1),
-        .play_rate_hz = 8000,
-        .data = primitive_1,
-    },
-
-    {
-        .effect_id = 2,
-        .length = ARRAY_SIZE(primitive_1),
-        .play_rate_hz = 8000,
-        .data = primitive_1,
-    },
-};
-
-static const struct effect_stream primitives_gentle[] = {
-    {
-        .effect_id = 0,
-        .length = ARRAY_SIZE(primitive_0),
-        .play_rate_hz = 8000,
-        .data = primitive_0,
-    },
-
-    {
-        .effect_id = 1,
-        .length = ARRAY_SIZE(primitive_1_gentle),
-        .play_rate_hz = 8000,
-        .data = primitive_1_gentle,
-    },
-
-    {
-        .effect_id = 2,
-        .length = ARRAY_SIZE(primitive_1_gentle),
-        .play_rate_hz = 8000,
-        .data = primitive_1_gentle,
-    },
-};
 
 const struct effect_stream* find_effect(const struct effect_stream* arr, size_t size, uint32_t effect_id) {
     for (size_t i = 0; i < size; ++i) {
